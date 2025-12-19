b6436e3fcdf6da50c1808750f6039f2840cd84cf
Mikhail <muxazila@mail.ru>
2025-12-19 20:45:13 +0700

    start

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,17 +148,14 @@ USE_I18N = True
 
 USE_TZ = True
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "../static/staticfiles")
-static_prefix = "../static"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Для collectstatic
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Ваши пользовательские статические файлы
+]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "media/"
-logger.debug(MEDIA_ROOT)
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
